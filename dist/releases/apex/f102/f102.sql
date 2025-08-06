b3798c926e437d6db3e9ec39c1260287f3d00f90
Prabhakar <prabhakar.lnu@oracle.com>
2025-08-06 13:02:40 +0530

    demo2 after stage

--- dist/releases/apex/f102/f102.sql
+++ dist/releases/apex/f102/f102.sql
@@ -155,7 +155,7 @@ wwv_imp_workspace.create_flow(
 ,p_substitution_value_04=>'Customers'
 ,p_file_prefix => nvl(wwv_flow_application_install.get_static_app_file_prefix,'')
 ,p_files_version=>31
-,p_version_scn=>39433067479943
+,p_version_scn=>39433303727843
 ,p_print_server_type=>'INSTANCE'
 ,p_file_storage=>'DB'
 ,p_is_pwa=>'N'
@@ -2210,7 +2210,7 @@ wwv_flow_imp_shared.create_security_scheme(
 ,p_scheme_type=>'NATIVE_FUNCTION_BODY'
 ,p_attribute_01=>'return true;'
 ,p_error_message=>'Insufficient privileges, user is not an Administrator'
-,p_version_scn=>39433067439152
+,p_version_scn=>39433303638226
 ,p_caching=>'BY_USER_BY_SESSION'
 );
 end;
@@ -2222,15 +2222,11 @@ wwv_flow_imp_shared.create_security_scheme(
 ,p_name=>'APPLICATION SENTRY'
 ,p_scheme_type=>'NATIVE_FUNCTION_BODY'
 ,p_attribute_01=>wwv_flow_string.join(wwv_flow_t_varchar2(
-'begin',
-'    if :APP_PAGE_ID = 101 then',
-'        return true;',
-'    else',
-'        return (eba_cust.get_authorization_level(:APP_USER) > 0);',
-'    end if;',
-'end;'))
+'return true;',
+'        ',
+''))
 ,p_error_message=>'You are not authorized to view this application, either because you have not been granted access, or your account has been locked. Please contact the application administrator.'
-,p_version_scn=>1089051550
+,p_version_scn=>39433303607789
 ,p_caching=>'BY_USER_BY_SESSION'
 );
 end;
@@ -12258,13 +12254,12 @@ wwv_flow_imp_page.create_page_plug(
 );
 wwv_flow_imp_page.create_page_plug(
  p_id=>wwv_flow_imp.id(19999519090210451317)
-,p_plug_name=>'More Filters _clone_hi'
+,p_plug_name=>'More Filters'
 ,p_region_template_options=>'#DEFAULT#:is-collapsed:t-Region--scrollBody:t-Form--stretchInputs'
 ,p_plug_template=>2664334895415463485
 ,p_plug_display_sequence=>126
 ,p_include_in_reg_disp_sel_yn=>'Y'
 ,p_plug_display_point=>'REGION_POSITION_02'
-,p_location=>null
 ,p_attributes=>wwv_flow_t_plugin_attributes(wwv_flow_t_varchar2(
   'expand_shortcuts', 'N',
   'output_as', 'HTML')).to_clob
@@ -33877,7 +33872,7 @@ wwv_flow_imp_page.create_page_item(
 ,p_item_sequence=>20
 ,p_item_plug_id=>wwv_flow_imp.id(14887231908134943557)
 ,p_prompt=>'Search'
-,p_placeholder=>'Search Customers'
+,p_placeholder=>'Search Customers in my demo2 branch...'
 ,p_display_as=>'NATIVE_TEXT_FIELD'
 ,p_cSize=>64
 ,p_cMaxlength=>4000
@@ -74065,4 +74060,4 @@ prompt  ...done
 
 
 
--- sqlcl_snapshot {"hash":"69563fd006b73473ad794b5337f1219d6e0f70c3","type":"APEX_APPLICATIONS","name":"f102","schemaName":"PRLNU","sxml":""}
+-- sqlcl_snapshot {"hash":"1186391ecbdf151368e0463f5cec0a46a170460a","type":"APEX_APPLICATIONS","name":"f102","schemaName":"PRLNU","sxml":""}
