c8df5bf6d92f04886cc58e01a7f610923a8e1340
Prabhakar <prabhakar.lnu@oracle.com>
2025-08-29 02:04:48 +0530

    poc_demo after project stage

--- dist/releases/apex/f102/f102.sql
+++ dist/releases/apex/f102/f102.sql
@@ -155,7 +155,7 @@ wwv_imp_workspace.create_flow(
 ,p_substitution_value_04=>'Customers'
 ,p_file_prefix => nvl(wwv_flow_application_install.get_static_app_file_prefix,'')
 ,p_files_version=>34
-,p_version_scn=>39441717715913
+,p_version_scn=>39441719099011
 ,p_print_server_type=>'INSTANCE'
 ,p_file_storage=>'DB'
 ,p_is_pwa=>'N'
@@ -34149,7 +34149,7 @@ wwv_flow_imp_page.create_page_item(
 ,p_item_sequence=>50
 ,p_item_plug_id=>wwv_flow_imp.id(14887231908134943557)
 ,p_item_default=>'0'
-,p_prompt=>'Geography'
+,p_prompt=>'Geography poc_demo'
 ,p_display_as=>'NATIVE_SELECT_LIST'
 ,p_named_lov=>'GEOGRAPHY'
 ,p_lov=>wwv_flow_string.join(wwv_flow_t_varchar2(
@@ -34330,7 +34330,7 @@ wwv_flow_imp_page.create_page_item(
 ,p_item_sequence=>40
 ,p_item_plug_id=>wwv_flow_imp.id(14887231908134943557)
 ,p_item_default=>'0'
-,p_prompt=>'Product'
+,p_prompt=>'Product demo_poc'
 ,p_display_as=>'NATIVE_SELECT_LIST'
 ,p_named_lov=>'PRODUCTS'
 ,p_lov=>wwv_flow_string.join(wwv_flow_t_varchar2(
@@ -74359,4 +74359,4 @@ prompt  ...done
 
 
 
--- sqlcl_snapshot {"hash":"0326e27fec5c534cde2854efd75196bf79858244","type":"APEX_APPLICATIONS","name":"f102","schemaName":"PRLNU","sxml":""}
+-- sqlcl_snapshot {"hash":"761a59519f5744c2364447b394b009d76fbc5e7a","type":"APEX_APPLICATIONS","name":"f102","schemaName":"PRLNU","sxml":""}
